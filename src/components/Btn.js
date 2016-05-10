e308a6088105e063030a6473dae5b2ba263b05fc
komcal <kom.sujautra@gmail.com>
2016-05-11 02:15:06 +0700

    add Mixin

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react'
 
-class Btn extends Component {
-  constructor () {
-    super()
+let Mixin = InnerComponent => class Mixed extends Component {
+  constructor (props) {
+    super(props)
     this.state = {
       value: 0
     }
     this.update = this.update.bind(this)
   }
   update () {
-    this.setState({value: this.state.value + 1})
+    this.setState({value: this.state.value + 1*this.state.m})
   }
   componentWillMount () {
     console.log('Mounting')
-    this.setState({m: 2})
+    this.setState({m: this.props.m})
   }
   componentDidMount () {
     console.log('Mounted')
@@ -29,12 +29,28 @@ class Btn extends Component {
   componentDidUpdate (prevProps, prevState) {
     console.log('prevState', prevState)
   }
+  render() {
+    return <InnerComponent
+      update={this.update}
+      {...this.state}
+      {...this.props} />
+  }
+}
+const Button = (props) => <button
+                            onClick={props.update}>
+                            {props.txt} - {props.value}
+                          </button>
+
+let ButtonMixed = Mixin(Button)
+let TestMixed = Mixin(Button)
+class Btn extends Component {
   render () {
     console.log('rendering')
     return (
-      <button onClick={this.update}>
-        {this.state.value * this.state.m}
-      </button>
+      <div>
+        <ButtonMixed txt='Button' m={2}/>
+        <TestMixed txt='Test' m={3}/>
+      </div>
     )
   }
 }
